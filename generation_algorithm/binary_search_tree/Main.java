dc0913edf120c3902e32e91d89651c4bc215a08e
Lương Chí Dũng <dungpubgame@gmail.com>
2025-03-05 11:15:22 +0700

    Create Main.java

---
+++ generation_algorithm/binary_search_tree/Main.java
@@ -0,0 +1,96 @@
+package binary_search_tree;
+
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node (int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class NodeManage {
+    public static Node insert(Node root, int x) {
+        if (root == null) {
+            return new Node(x);
+        }
+        if (root.data > x) {
+            root.left = insert(root.left, x);
+        }
+        else if (root.data < x) {
+            root.right = insert(root.right, x);
+        }
+        return root;
+    }
+    public static void preorder(Node root) {
+        if(root != null){
+            preorder(root.left);
+            System.out.print(root.data +  " ");
+            preorder(root.right);
+        }
+    }
+    public static boolean nodeSearch(Node root, int x) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == x) {
+            return true;
+        }
+        else {
+            if (x < root.data) {
+                return nodeSearch(root.left, x);
+            }
+            else {
+                return nodeSearch(root.right, x);
+            }
+        }
+    }
+    public Node minNode(Node root) {
+        Node tmp = root;
+        while (tmp != null && tmp.left != null) {
+            tmp = tmp.left;
+        }
+        return tmp;
+    }
+    public static Node nodeDelete(Node root, int x) {
+        if (root == null) {
+            return root;
+        }
+        if (x < root.data) {
+            root.left =  nodeDelete(root.left, x);
+        }
+        else if(x > root.data) {
+            root.right = nodeDelete(root.right, x);
+        }
+        else {
+            if (root.left == null || root.right == null) {
+                return root.right;
+            }
+            else {
+                
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        Node root = null;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i = 1; i <= n; i ++) {
+            int tmp;
+            tmp = sc.nextInt();
+            root = NodeManage.insert(root, tmp);
+        }
+        NodeManage.preorder(root);
+        boolean check = NodeManage.nodeSearch(root, 2);
+        System.out.println(check);
+    }
+}
